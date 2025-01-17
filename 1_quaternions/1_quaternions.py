c3f29b4ee39df91e07a0d7c50cb23c2f2397a7ba
ILOveMyasko <brutalkin_v@mail.ru>
2025-01-17 12:26:15 +0300

    added divisions of two quat

--- 1_quaternions/1_quaternions.py
+++ 1_quaternions/1_quaternions.py
@@ -1,6 +1,7 @@
 ﻿
 import numpy as np
 import math
+#Class Quaternion 
 
 class Quaternion:
     def __init__(self, w, x, y, z):
@@ -14,18 +15,34 @@ class Quaternion:
 
     def __add__(self, other):
         if isinstance(other, Quaternion):
-            return Quaternion(self.w + other.w, self.x + other.x, self.y + other.y, self.z + other.z)
+            return Quaternion(
+            self.w + other.w,
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z)
         elif isinstance(other, (int, float)):
-            return Quaternion(self.w + other, self.x, self.y, self.z)
+            return Quaternion(
+            self.w + other,
+            self.x,
+            self.y,
+            self.z)
         else:
             return NotImplemented
 
     def __sub__(self, other):
         """Quat. substraction"""
         if isinstance(other, Quaternion):
-            return Quaternion(self.w - other.w, self.x - other.x, self.y - other.y, self.z - other.z)
+            return Quaternion(
+            self.w - other.w,
+            self.x - other.x,
+            self.y - other.y,
+            self.z - other.z)
         elif isinstance(other, (int, float)):
-            return Quaternion(self.w - other, self.x, self.y, self.z)
+            return Quaternion(
+            self.w - other,
+            self.x,
+            self.y,
+            self.z)
         else:
             return NotImplemented
 
@@ -38,28 +55,31 @@ class Quaternion:
             z = self.w * other.z + self.x * other.y - self.y * other.x + self.z * other.w
             return Quaternion(w, x, y, z)
         elif isinstance(other, (int, float)):
-            return Quaternion(self.w * other, self.x * other, self.y * other, self.z * other)
-        else:
-            return NotImplemented
-
-    def __truediv__(self, other):
-        """деление на вещественное число"""
-        if isinstance(other, (int, float)):
-            if other == 0:
-                raise ZeroDivisionError("Division by zero is not allowed for quaternions.")
-            # Деление кватерниона на вещественное число
-            return Quaternion(self.w / other, self.x / other, self.y / other, self.z / other)
+            return Quaternion(
+                self.w * other,
+                self.x * other,
+                self.y * other, 
+                self.z * other)
         else:
             return NotImplemented
 
     def __neg__(self):
         """minus"""
-        return Quaternion(-self.w, -self.x, -self.y, -self.z)
+        return Quaternion(
+            -self.w, 
+            -self.x, 
+            -self.y, 
+            -self.z)
 
     def __pos__(self):
         """self"""
-        return Quaternion(self.w, self.x, self.y, self.z)
+        return Quaternion(
+            self.w, 
+            self.x, 
+            self.y, 
+            self.z)
 
+    # Properties
     @property
     def norm(self):
         """quat. norm"""
@@ -68,7 +88,11 @@ class Quaternion:
     @property
     def conjugate(self):
         """conjugation"""
-        return Quaternion(self.w, -self.x, -self.y, -self.z)
+        return Quaternion(
+            self.w, 
+            -self.x, 
+            -self.y, 
+            -self.z)
 
     @property
     def inverse(self):
@@ -78,27 +102,51 @@ class Quaternion:
             self.conjugate.w / norm_sq,
             self.conjugate.x / norm_sq,
             self.conjugate.y / norm_sq,
-            self.conjugate.z / norm_sq
-        )
+            self.conjugate.z / norm_sq)
+
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            if other == 0:
+               raise ZeroDivisionError("Division by zero is not allowed for quaternions.")
+            return Quaternion(self.w / other, self.x / other, self.y / other, self.z / other)
+    
+        elif isinstance(other, Quaternion):
+            print(other)
+            if other.norm == 0:
+                raise ZeroDivisionError("Division by zero is not allowed for quaternions.")
+            conjugated = other.conjugate
+            norm_squared = other.norm ** 2
+            # q1/q2 = q1 * (q2.conjugated) / q2.norm
+            return (self*conjugated)/norm_squared
+        else:
+            return NotImplemented
+    
+    
+    def __str__(self):
+     """String representation in a format of: a + bi + cj + dk respecting signs.
+    """
+     return f"({'- ' if self.w < 0 else ''}{abs(self.w)} " \
+            f"{'+' if self.x >= 0 else '-'} {abs(self.x)}i " \
+            f"{'+' if self.y >= 0 else '-'} {abs(self.y)}j " \
+            f"{'+' if self.z >= 0 else '-'} {abs(self.z)}k)"
 
     def rotate_vector(self, vector):
         #resultVector = q * originalVector * q'; here q is our original quaternion(self)
-        q_vector = Quaternion(0, vector[0], vector[1], vector[2])
-        rotated_q = self * q_vector * self.inverse
+        original_vector = Quaternion(0, vector[0], vector[1], vector[2])
+        rotated_q = self * original_vector * self.inverse
         return (rotated_q.x, rotated_q.y, rotated_q.z)
 
     @staticmethod
     def from_axis_angle(axis, angle):
         """Create a rotation quaternion based on an axis and an angle"""
         half_angle = angle / 2
-        norm =  math.sqrt(axis[0]**2 + axis[1]**2 + axis[2]**2)
+        norm = Quaternion(0,axis).norm
         sin_half_angle = math.sin(half_angle)
         return Quaternion(
             math.cos(half_angle),
             axis[0] / norm * sin_half_angle,
             axis[1] / norm * sin_half_angle,
-            axis[2] / norm * sin_half_angle
-        )
+            axis[2] / norm * sin_half_angle)
     
     def __eq__(self, other):
         """compare"""
@@ -107,26 +155,13 @@ class Quaternion:
                 math.isclose(self.w, other.w) and
                 math.isclose(self.x, other.x) and
                 math.isclose(self.y, other.y) and
-                math.isclose(self.z, other.z)
-            )
+                math.isclose(self.z, other.z))
         elif isinstance(other, (int, float)):
-            """comare to int/float"""
+            """compare to int/float"""
             return math.isclose(self.w, other) and math.isclose(self.x, 0) and math.isclose(self.y, 0) and math.isclose(self.z, 0)
         else:
             return False
 
-    def is_real(self):
-        return math.isclose(self.x, 0) and math.isclose(self.y, 0) and math.isclose(self.z, 0)
-
-    def is_pure_imaginary(self):
-        return math.isclose(self.w, 0) and (not math.isclose(self.x, 0) or not math.isclose(self.y, 0) or not math.isclose(self.z, 0))
-
-    def is_complex(self):
-        return not math.isclose(self.w, 0) and (not math.isclose(self.x, 0) or not math.isclose(self.y, 0) or not math.isclose(self.z, 0))
-
-    def __str__(self):
-        """to_string"""
-        return f"({'- ' if self.w < 0 else ''}{abs(self.w)} {'+' if self.x >= 0 else '-'} {abs(self.x)}i {'+' if self.y >= 0 else '-'} {abs(self.y)}j {'+' if self.z >= 0 else '-'} {abs(self.z)}k)"
 
 if __name__ == "__main__":
 
@@ -137,9 +172,10 @@ if __name__ == "__main__":
     print()
     print(f"{'q1'}     {q1}")
     print(f"{'q2'}     {q2}")
-    print()
+    print(q1==q2)
     print(f"{'q1 * 2'}     {q1 * 2}")
     print(f"{'q1 / 2'}     {q1 / 2}")
+    print(f"{'q1 / q2'}     {q1 / q2}")
     print(f"{'q2 - 2.2'}     {q2 - 2.2}")
     print(f"{'q2 + 0.2'}     {q2 + 0.2}")
     print()
@@ -148,12 +184,11 @@ if __name__ == "__main__":
     print(f"{'q1 * q2'}     {q1 * q2}")
     print()
     print(f"{'-q1'}     {-q1}")
-    print(f"{'Conjugation for q2'}     {q2.conjugate}")
+    print(f"{'Conjugated for q2'}     {q2.conjugate}")
     print(f"{'q2 Norm'}     {q2.norm}")
     print()
-    print(f"q2 * q2^-1 == 1  : {q2 * q2.inverse == 1}")
-    print(f"Check if q2 is pure Im : {q2.is_pure_imaginary()}")
-    print(f"Check if (1+i) complex : {Quaternion(1,1,0,0).is_complex()}")
+    print(f"q2 * q2.inversed == 1  : {q2 * q2.inverse == 1}")
     print()
+    print(q2.__str__())
     print(f"Rotate : {q2.rotate_vector(vector=[1,2,3])}")
    
